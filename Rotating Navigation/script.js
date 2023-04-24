6ba0209339284133fc764272c1f93fc4fe8b83fc
MUKONGE ERIC <mukonge.eric@gmail.com>
2023-04-24 23:48:08 +0300

    Update

--- Rotating Navigation/script.js
+++ Rotating Navigation/script.js
@@ -1,6 +1,6 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
-const container = document.querySelector('.container')
+const container = document.querySelectorAll('.container')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
